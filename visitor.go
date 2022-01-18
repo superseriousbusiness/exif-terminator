aa4155feb85381c692c1a09c053d6c25d71a162b
tsmethurst <tobi.smethurst@protonmail.com>
2022-01-18 23:11:18 +0100

    testing visitor function

--- visitor.go
+++ visitor.go
@@ -2,21 +2,32 @@ package terminator
 
 import (
 	"fmt"
+	"io"
 
 	jpegstructure "github.com/dsoprea/go-jpeg-image-structure/v2"
 )
 
 type jpegVisitor struct {
-	js *jpegstructure.JpegSplitter
+	js     *jpegstructure.JpegSplitter
+	writer io.Writer
 }
 
 func (v *jpegVisitor) HandleSegment(markerId byte, markerName string, counter int, lastIsScanData bool) error {
 	segmentList := v.js.Segments()
 	segments := segmentList.Segments()
-	lastSegment := segments[counter]
+	lastSegment := segments[len(segments)-1]
+
 	fmt.Println(markerId, markerName, counter, lastIsScanData, lastSegment.IsExif())
 	if lastSegment.IsExif() {
-		fmt.Println(lastSegment.Data)
+		zeroed := make([]byte, len(lastSegment.Data))
+		fmt.Printf("writing %d bytes over exif data of length %d\n", len(zeroed), len(lastSegment.Data))
+		n, err := v.writer.Write(zeroed)
+		fmt.Printf("wrote %d\n", n)
+		return err
 	}
-	return nil
+	
+
+	n, err := v.writer.Write(lastSegment.Data)
+	fmt.Printf("wrote %d\n", n)
+	return err
 }
