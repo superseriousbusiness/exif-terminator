9c97d7447b0a382b1949edc2945eeac6bb8848cb
tsmethurst <tobi.smethurst@protonmail.com>
2022-06-22 20:00:42 +0200

    add panorama test

--- terminator_test.go
+++ terminator_test.go
@@ -162,6 +162,38 @@ func (suite *TerminatorTestSuite) TestTerminateTurnip() {
 	suite.EqualValues(turnipClean, b)
 }
 
+func (suite *TerminatorTestSuite) TestTerminatePanorama() {
+	turnip, err := os.Open("./images/exif-panorama.jpg")
+	if err != nil {
+		panic(err)
+	}
+
+	stat, err := turnip.Stat()
+	if err != nil {
+		panic(err)
+	}
+
+	out, err := terminator.Terminate(turnip, int(stat.Size()), "jpeg")
+	suite.NoError(err)
+
+	// we should be able to get some bytes back from the returned reader
+	b, err := io.ReadAll(out)
+	suite.NoError(err)
+	suite.NotEmpty(b)
+
+	// the processed image should have the same size as the initial image
+	suite.EqualValues(stat.Size(), len(b))
+
+	// should be decodable as a jpeg
+	_, err = jpeg.Decode(bytes.NewBuffer(b))
+	suite.NoError(err)
+
+	// bytes should be the same as the clean image
+	err = os.WriteFile("./images/exif-panorama-clean.jpg", b, 0666)
+	suite.NoError(err)
+	// suite.EqualValues(turnipClean, b)
+}
+
 func TestTerminatorTestSuite(t *testing.T) {
 	suite.Run(t, &TerminatorTestSuite{})
 }
